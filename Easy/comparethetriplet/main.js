eb4c7a85a6289d1d27f77108101d5049d1005d97
evandri30 <evandriridhoh@gmail.com>
2026-06-22 19:14:09 +0700

    add how i think before solved problem

--- Easy/comparethetriplet/main.js
+++ Easy/comparethetriplet/main.js
@@ -1,17 +1,28 @@
+/*
+    1. Butuh input dua array (alice & bob)
+    2. Siapin score awal buat keduanya (mulai dari 0)
+    3. Cek apakah panjang array sama
+    4. Loop, bandingin nilai alice[i] sama bob[i]
+    5. Siapa yang lebih gede, dia dapet poin
+    6. Return kedua score dalam bentuk array
+*/
+
 const alice = [5, 6, 7]
 const bob = [3, 6, 10]
 
-const compareScore = (a,b) => {
+const compareScore = (alice,bob) => {
     let aliceScore = 0
     let bobScore = 0
 
-    for(let i = 0; i < a.length; i++) {
-        if(a[i] > b[i]) {
+    if (alice.length !== bob.length) {
+        return `Array length not same`
+    }
+
+    for(let i = 0; i < alice.length; i++) {
+        if(alice[i] > bob[i]) {
             aliceScore = aliceScore + 1
-            console.log(aliceScore)
-        } else if(b[i] > a[i]) {
+        } else if(bob[i] > alice[i]) {
             bobScore = bobScore + 1
-            console.log(bobScore)
         }
     }
 
